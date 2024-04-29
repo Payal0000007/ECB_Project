9c25e4562fa2ac27e86f5e0705554134a02936d7
payal <3payalr@gmail.com>
2024-04-29 15:25:26 +0530

    changes

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { AppModule } from './app.module';
 import { BadGatewayException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true,exceptionFactory(errors) {
     return new BadGatewayException({
       message:'Validation Failed',
@@ -23,37 +23,4 @@ async function bootstrap() {
   await app.listen(8000);
 }
 bootstrap();
-// import { NestFactory } from '@nestjs/core';
-// import { AppModule } from './app.module';
-// import { BadGatewayException, ValidationPipe } from '@nestjs/common';
-
-// async function bootstrap() {
-//   const app = await NestFactory.create(AppModule);
-  
-//   // Set up global validation pipe
-//   app.useGlobalPipes(new ValidationPipe({ 
-//     stopAtFirstError: true,
-//     exceptionFactory(errors) {
-//       return new BadGatewayException({
-//         message: 'Validation Failed',
-//         errors: errors
-//       });
-//     },
-//   }));
-
-//   // Enable CORS
-//   app.enableCors({
-//     origin: '*',
-//     methods: ['GET', 'PUT', 'POST', 'DELETE', 'PATCH'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//     preflightContinue: false,
-//     optionsSuccessStatus: 200,
-//     credentials: true,
-//     maxAge: 3600,
-//   });
-
-//   await app.listen(8000);
-// }
-
-// bootstrap();
 
